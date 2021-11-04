aebe429b67a2ad2bae90958f2d1945368a540fe7
Blake Guidry <guidry.bd@gmail.com>
2021-11-03 20:14:42 -0500

    Add local setting; add requirements.txt

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blakeguidry.pythonanywhere.com']
 
 
 # Application definition
@@ -130,3 +130,8 @@ MEDIA_ROOT = Path(BASE_DIR / 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('No local settings found')
